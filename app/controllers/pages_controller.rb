9f116b65fb7049d5403447f7c5a951c88c924abf
Marcelo Almeida <contact@marcelopazzo.com>
2026-04-21 16:06:43 -0300

    feat: add pages controller views, layout, header and footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,19 @@
 class PagesController < ApplicationController
   def home
-    render plain: "ok"
   end
 
   def quickstart
-    render plain: "ok"
   end
 
   def algorithms
-    render plain: "ok"
   end
 
   def hybrid
-    render plain: "ok"
   end
 
   def security
-    render plain: "ok"
   end
 
   def debugger
-    render plain: "ok"
   end
 end
